bb12a6f7d42a9f9ba8579e3105a46ca4b8a62d4b
karim-rkha <karimrkha@gmail.com>
2022-09-18 10:33:15 +0200

    ML_Data_V1

--- Data/Make_ML_data.py
+++ Data/Make_ML_data.py
@@ -49,28 +49,44 @@ sort_WL_Minters = sorted(res_WL_Minters.items(), key=lambda x: x[1], reverse=Tru
 
 # print(sort_WL_Minters)
 
-col=[]
-for tuple in sort_WL_Minters:
-    col.append(tuple[0])
+# col=[]
+# for tuple in sort_WL_Minters:
+#     col.append(tuple[0])
+col=['Wallets','Minter','Tezotopia Resources','GIF DAO', 'Diplomat NFTs', 'Tezotopia NFT Registry', 'Rocket Monsters', 'Tezzles', 'FLAME', 'Tezos Degen Club', 'Froggos', 'VesselsGen0', 'Mekatron K9', 'Fellowship of Ilmeresh', 'Oxtazy', 'crDAO', 'PXL', 'PandaCoin', 'WRAP Governance Token', 'DOGAMÍ NFTs', 'Wrapped Tokens Contract', 'PixelDebates Tokens', 'PixelPotus 1.0', 'DOGAMÍ Doga Bones', 'CRUNCH', 'GAP Threads Asset Manager', 'Chop Sumo', 'Bunny Knights 2nd Gen', 'QUIPU', 'Pixel Panda', 'Youves Synthetic Asset', 'WTZ', 'OWLZ', 'Mooncakes', 'SPI', 'Minterpop Multi Asset Manager 2', 'Atelier 09', 'SpacePunky', 'Salsa', 'Papa Johns Hot Bags', 'KALAM', 'TezApeGang', 'pxlDAO', 'CUBE HEAD collaborations', 'Codename: AK1RA', 'WALLY and his Wonderful Wardrobe', 'PixelPotus 2.0', 'Flux Tribe', 'Tezzles Tributes', 'Kalamint Art House', 'Minterpop Multi Asset Manager 1', 'tzDropz', 'YOU Governance', 'sCasino Tokens', 'Materia', 'hDAO', 'Peace for Ukraine', 'TezDAO', 'FXHASH GENTK', 'PixelPotus Founders Edition', 'CR7 - TezoTrooperz Tribute', 'Ottez Extras', 'Ottez Oysters', 'Tezos Domains NameRegistry', 'Rarible', 'Ottez', 'akaSwap NFTs', 'hic et nunc NFTs', None, 'CyberKidz Club', 'Unibotz Collectables', 'Les Éléfants Terribles', 'Tezos Thugs', 'Versum Items', 'Ziggurats', 'Cyber Gecko Gang', 'GOGOs', 'The Moments', 'NEONZ', 'PRJKTNEON Token', 'Tezzardz', 'Lightfields', '8bidou 8x8', 'Skeletons, Etc.', 'Randomly Common Skeles']
 
-df = pd.DataFrame(columns=['Name','Number of WL spots','Percentage of the WL','Conversion rate','Number of minters'])
-
-df.loc[0]=['Overall',2400,'100%','11.21%',len(minters)]
+df = pd.DataFrame(columns=col)
 
 
 
 
 i=0
-for x in d:
-    name=col[i]
-    if res_WL[name]>=1:
-        df.loc[i+1] = [name,len(d[name]['WL']),str(res_WL[name])+"%",str(res_WL_Minters[name])+"%",len(d[name]['WL_Minters'])]
+for w in WL:
+    l=[w]
+    if w in minters:
+        l.append(1)
+    else:
+        l.append(0)
+    j=0
+    for c in col:
+        if j>1:
+            if c in inv_WL[w]:
+                l.append(1)
+            else:
+                l.append(0)
+        j+=1
+    df.loc[i]=l
     i+=1
 
-print(df)
+# for x in d:
+#     name=col[i]
+#     if res_WL[name]>=3:
+#         df.loc[i+1] = [name,len(d[name]['WL']),str(res_WL[name])+"%",str(res_WL_Minters[name])+"%",len(d[name]['WL_Minters'])]
+#     i+=1
+
+# print(df.head())
 
 
-with open("/Users/rkhachahamkarim/Documents/GitHub/Dashboard_Tezotrooperz/App/df", "wb") as fp:  # Pickling
+with open("/Users/rkhachahamkarim/Documents/GitHub/Dashboard_Tezotrooperz/App/ml_df", "wb") as fp:  # Pickling
     pickle.dump(df, fp)
 
 
